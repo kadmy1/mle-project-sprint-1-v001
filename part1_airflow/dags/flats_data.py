82044b0642be6a360a93bf42ad812acd47ef1300
kadmy1 <kadmy-kadmy@yandex.ru>
2024-08-14 07:23:08 +0000

    outliers duplicates

--- part1_airflow/dags/flats_data.py
+++ part1_airflow/dags/flats_data.py
@@ -87,7 +87,31 @@ def prepare_flat_dataset():
 
     @task()
     def transform(data: pd.DataFrame):
-        # at sql
+        def remove_duplicates(data):
+            feature_cols = data.columns.drop('flat_id').tolist()
+            is_duplicated_features = data.duplicated(subset=feature_cols, keep=False)
+            data = data[~is_duplicated_features].reset_index(drop=True)
+            return data
+
+        def remove_outliers(data):
+            num_cols = data.select_dtypes(['float']).columns
+            threshold = 1.5
+            potential_outliers = pd.DataFrame()
+
+            for col in num_cols:
+                Q1 = data[col].quantile(0.25)
+                Q3 = data[col].quantile(0.75)
+                IQR = Q3 - Q1
+                margin = threshold * IQR
+                lower = Q1 - margin
+                upper = Q3 + margin
+                potential_outliers[col] = ~data[col].between(lower, upper)
+            outliers = potential_outliers.any(axis=1)
+            data = data[~outliers].reset_index(drop=True)
+            return data
+
+        data = remove_duplicates(data)
+        data = remove_outliers(data)
         return data
 
     @task()
